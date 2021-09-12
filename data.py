d963d3a8045dde4dfcc87013ba6a2f49e9cd21c1
WeijieChen2017 <weijiechen1997@gmail.com>
2021-09-12 18:37:05 -0500

    starter build

---
+++ data.py
@@ -0,0 +1,27 @@
+ from torch.utils.data import Dataset
+
+ class DatasetFromFolder(data.Dataset):
+    def __init__(self, data_dir_X, data_dir_Y, batch_size, shuffle=False):
+        super(DatasetFromFolder, self).__init__()
+        self.filenames_X = sorted(glob(os.path.join(data_dir_X,'*.npy'),recursive=True))
+        self.filenames_Y = sorted(glob(os.path.join(data_dir_Y,'*.npy'),recursive=True))
+        self.batch_size = batch_size
+        self.shuffle = shuffle
+
+        if shuffle:
+            temp = list(zip(self.filenames_X, self.filenames_Y))
+            random.shuffle(temp)
+            self.filenames_X, self.filenames_Y = zip(*temp)
+
+    def __getitem__(self, index):
+
+        batch_x_fns = self.filenames_X[idx * self.batch_size:(idx + 1) * self.batch_size]
+        batch_y_fns = self.filenames_Y[idx * self.batch_size:(idx + 1) * self.batch_size]
+
+        batch_x = np.array( [ np.load(fn) for fn in batch_x_fns ] )
+        batch_y = np.array( [ np.load(fn) for fn in batch_y_fns ] )
+
+        return batch_x, batch_y
+
+    def __len__(self):
+        return len(self.filenames_X // self.batch_size)
--- train.py
+++ train.py
@@ -1,8 +1,69 @@
 from model import Net
+from __future__ import print_function
+import argparse
+
 import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader
+
+from data import DatasetFromFolder
+
+# training setting
+parser = argparse.ArgumentParser(description='Use 3d Unet to translate NAC PET to CT')
+parser.add_argument('--batch_size', type=int, default=4, help='training batch size')
+parser.add_argument('--test_batch_size', type=int, default=4, help='testing batch size')
+parser.add_argument('--epochs', type=int, default=10, help='number of epochs to train for')
+parser.add_argument('--lr', type=float, default=0.01, help='Learning Rate. Default=0.01')
+parser.add_argument('--data_worker', type=int, default=4, help='number of threads for data loader to use')
+parser.add_argument('--seed', type=int, default=813, help='random seed to use.')
+parser.add_argument('--block_size', type=int, default=64, help='the block size of each input')
+parser.add_argument('--stride', type=int, default=32, help='the stride in dataset')
+parser.add_argument('--depth', type=int, default=2, help='the depth of unet')
+parser.add_argument('--num_filters', type=int, default=16, help='the number of starting filters')
+opt = parser.parse_args()
+print(opt)
+
+# basic setting
+torch.manual_seed(opt.seed)
+device = torch.device("cuda" if opt.cuda else "cpu")
+
+# set the dataset
+trainFolderX = "./data_train/X/train/"
+trainFolderY = "./data_train/Y/train/"
+testFolderX = "./data_train/X/test/"
+testFolderY = "./data_train/Y/test/"
+valFolderX = "./data_train/X/val/"
+valFolderY = "./data_train/Y/val/"
+
+dataset_train = DatasetFromFolder(data_dir_X = trainFolderX,
+                                  data_dir_Y = trainFolderY,
+                                  batch_size = opt.batch_size)
+dataset_val = DatasetFromFolder(data_dir_X = valFolderX,
+                                data_dir_Y = valFolderY,
+                                batch_size = opt.batch_size)
+
+dataloader_train = DataLoader(dataset=dataset_train,
+                              num_workers=opt.data_worker,
+                              batch_size=opt.batch_size,
+                              shuffle=True)
+
+dataloader_val = DataLoader(dataset=dataset_val,
+                            num_workers=opt.data_worker,
+                            batch_size=opt.batch_size,
+                            shuffle=True)
+
+# build the network
+model = Net(block_size = opt.block_size,
+            num_filters = opt.num_filters,
+            num_level = opt.depth).to(device)
+for param in model.parameters():
+    print(type(param), param.size())
+criterion = nn.HuberLoss
+optimizer = optim.Adam(model.parameters(), lr=opt.lr)
 
 
-net = Net(block_size=64, num_filters=16, num_level=2)
+
 
 # (N,C,D,H,W)
 input = torch.randn(4, 1, 64, 64, 64)
