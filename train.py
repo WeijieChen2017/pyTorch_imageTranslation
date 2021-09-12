62990bcdf117c3696a3edccf5ca02dd21fdc70dc
WeijieChen2017 <weijiechen1997@gmail.com>
2021-09-12 02:55:43 -0500

    starter build

--- train.py
+++ train.py
@@ -2,7 +2,7 @@ from model import Net
 import torch
 
 
-net = Net(block_size=64, num_filters=16, num_level=2, verbose=True)
+net = Net(block_size=64, num_filters=16, num_level=3)
 
 # (N,C,D,H,W)
 input = torch.randn(4, 1, 64, 64, 64)
