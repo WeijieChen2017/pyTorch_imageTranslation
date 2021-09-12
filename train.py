90ae49f2c686b5865525f0b5048701eb257c31f3
WeijieChen2017 <weijiechen1997@gmail.com>
2021-09-12 02:09:37 -0500

    starter build

--- train.py
+++ train.py
@@ -4,9 +4,9 @@ from model import Net
 net = Net()
 print(net)
 
-params = list(net.parameters())
-print(len(params))
-print(params[0].size())
+# params = list(net.parameters())
+# print(len(params))
+# print(params[0].size())
 
 # (N,C,D,H,W)
 input = torch.randn(4, 1, 32, 32, 32)
