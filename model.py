ce45017685239aaa5a8b7c4b91985119a64db1f6
WeijieChen2017 <weijiechen1997@gmail.com>
2021-09-12 19:50:10 -0500

    starter build

--- model.py
+++ model.py
@@ -26,6 +26,7 @@ class Net(nn.Module):
 
     def forward(self, x):
         for layer in self.network_layer:
+            print(type(x.double()))
             x = layer(x.double())
         return x
 
