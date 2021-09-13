c5d7dfc5ae12a5839169e2807290938989777fb9
WeijieChen2017 <weijiechen1997@gmail.com>
2021-09-12 20:00:35 -0500

    starter build

--- train.py
+++ train.py
@@ -10,7 +10,7 @@ from data import DatasetFromFolder
 
 # training setting
 parser = argparse.ArgumentParser(description='Use 3d Unet to translate NAC PET to CT')
-parser.add_argument('--batch_size', type=int, default=1, help='training batch size')
+parser.add_argument('--batch_size', type=int, default=2, help='training batch size')
 parser.add_argument('--test_batch_size', type=int, default=4, help='testing batch size')
 parser.add_argument('--epochs', type=int, default=10, help='number of epochs to train for')
 parser.add_argument('--lr', type=float, default=0.01, help='Learning Rate. Default=0.01')
