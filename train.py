91fbc8d9aba0cfdc1f8f7406672b703f59d20142
WeijieChen2017 <weijiechen1997@gmail.com>
2021-09-17 14:25:49 -0500

    starter build

--- train.py
+++ train.py
@@ -9,9 +9,45 @@ from torch.utils.data import DataLoader
 from model import Net
 from data import DatasetFromFolder
 
+def train_a_epoch(data_loader, epoch, device, loss_batch_cnt):
+
+    epoch_loss = 0
+    epoch_loss_list = []
+    loss_batch = []
+    for iteration, batch in enumerate(data_loader, 1):
+        batch_x, batch_y = batch[0].to(device), batch[1].to(device)
+        # batch_x = torch.from_numpy(batch_x).double()
+        # batch_y = torch.from_numpy(batch_y).double()
+        # print(batch_x.size())
+        optimizer.zero_grad()
+        loss = criterion(model(batch_x), batch_y)
+        epoch_loss += loss.item()
+        loss.backward()
+        optimizer.step()
+        loss_batch.append(loss.item())
+        epoch_loss_list.append(loss.item())
+
+        if len(loss_batch) % loss_batch_cnt == 0:
+            loss_package = np.asarray(loss_batch)
+            loss_mean = np.mean(loss_package)
+            loss_std = np.std(loss_package)
+            print("===> Epoch[{}]({}/{}): {:.6f}".format(epoch, iteration, len(data_loader)), end='')
+            print("Loss mean: {:.6f}".format(loss_mean), " Loss std: ".format(loss_std))
+            loss_batch = []
+        
+    loss_package = np.asarray(loss_batch)
+    loss_mean = np.mean(loss_package)
+    loss_std = np.std(loss_package)
+    print("===> Epoch[{}]({}/{}): {:.6f}".format(epoch, iteration, len(data_loader)), end='')
+    print("Loss mean: {:.6f}".format(loss_mean), " Loss std: ".format(loss_std))
+
+    return epoch_loss_list
+
+
 # training setting
 parser = argparse.ArgumentParser(description='Use 3d Unet to translate NAC PET to CT')
-parser.add_argument('--batch_size', type=int, default=32, help='training batch size')
+parser.add_argument('--batch_size', type=int, default=64, help='training batch size')
+parser.add_argument('--loss_batch_cnt', type=int, default=32, help='loss display batch')
 parser.add_argument('--epochs', type=int, default=10, help='number of epochs to train for')
 parser.add_argument('--lr', type=float, default=0.001, help='Learning Rate. Default=0.01')
 parser.add_argument('--data_worker', type=int, default=8, help='number of threads for data loader to use')
@@ -20,7 +56,8 @@ parser.add_argument('--cpu', action='store_true', help='use cuda?')
 parser.add_argument('--block_size', type=int, default=32, help='the block size of each input')
 parser.add_argument('--stride', type=int, default=32, help='the stride in dataset')
 parser.add_argument('--depth', type=int, default=2, help='the depth of unet')
-parser.add_argument('--num_filters', type=int, default=48, help='the number of starting filters')
+parser.add_argument('--num_filters', type=int, default=16, help='the number of starting filters')
+parser.add_argument('--model_tag', type=str, default="b64s32d2f16", help='tag of the current model')
 opt = parser.parse_args()
 print(opt)
 
@@ -69,30 +106,25 @@ print("===> The network, loss, optimizer are set")
 
 # start the training
 
+val_loss_best = 1e6
 for epoch in range(opt.epochs):
 
-    epoch_loss = 0
-    epoch_loss_list = []
-    for iteration, batch in enumerate(dataloader_train, 1):
-        batch_x, batch_y = batch[0].to(device), batch[1].to(device)
-        # batch_x = torch.from_numpy(batch_x).double()
-        # batch_y = torch.from_numpy(batch_y).double()
-        # print(batch_x.size())
-        optimizer.zero_grad()
-        loss = criterion(model(batch_x), batch_y)
-        epoch_loss += loss.item()
-        loss.backward()
-        optimizer.step()
+    loss_list = np.asarray(train_a_epoch(dataloader_train, epoch, device, opt.loss_batch_cnt))
+    loss_mean = np.mean(epoch_loss_list)
+    loss_std = np.std(epoch_loss_list)
+    np.save("epoch_Loss_{}.npy".format(epoch), np.asarray(loss_list))
+    print("===> Epoch {} Complete Loss, Avg: {:.6f}, Std: {:.6f}".format(epoch, loss_mean, loss_std))
 
-        print("===> Epoch[{}]({}/{}): Loss: {:.6f}".format(epoch, iteration, len(dataloader_train), loss.item()))
-        epoch_loss_list.append(loss.item())
+    loss_list = np.asarray(train_a_epoch(dataloader_val, epoch, device, opt.loss_batch_cnt))
+    loss_mean = np.mean(epoch_loss_list)
+    loss_std = np.std(epoch_loss_list)
+    np.save("val{}.npy".format(epoch), np.asarray(loss_list))
+    print("===> Val {} Complete Loss, Avg: {:.6f}, Std: {:.6f}".format(epoch, loss_mean, loss_std))
 
-    np.save("epoch_Loss_{}.npy".format(epoch), np.asarray(epoch_loss_list))
-    print("===> Epoch {} Complete: Avg. Loss: {:.6f}".format(epoch, epoch_loss / len(dataloader_train)))
-
-    model_save_path = "model_epoch_{}.pth".format(epoch)
-    torch.save(model, model_save_path)
-    print("Checkpoint saved to {}".format(model_save_path))
+    if loss_mean < val_loss_best:
+        model_save_path = "model_best_{}.pth".format(opt.model_tag)
+        torch.save(model, model_save_path)
+        print("Checkpoint saved to {}".format(model_save_path))
 
 # # (N,C,D,H,W)
 # input = torch.randn(4, 1, 32, 32, 32).double()
