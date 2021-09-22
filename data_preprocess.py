38174e006e7ef4cd0e44e713f746ee22918167e3
WeijieChen2017 <weijiechen1997@gmail.com>
2021-09-21 23:57:02 -0500

    Good morning!

--- data_preprocess.py
+++ data_preprocess.py
@@ -43,7 +43,7 @@ testRatio = 0.1
 channelX = 1
 channelY = 1
 block_size = 64
-stride = 32
+stride = 16
 
 # create directory and search nifty files
 trainFolderX = "./data_train/X"+str(block_size)+"/train/"
