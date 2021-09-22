d9d006835a10b1afd4fbb0dfd7b24bccf90ff226
WeijieChen2017 <weijiechen1997@gmail.com>
2021-09-22 00:32:15 -0500

    Good morning!

--- data_preprocess.py
+++ data_preprocess.py
@@ -156,7 +156,8 @@ for package in [packageTest, packageVal, packageTrain]:
     # npy version
     for pathX in fileList:
         print(pathX, ' '*4, end='')
-        flag = dataLoaderPool.apply_async(save_each_nifty, (folderX, folderY, pathX))
+        oz = (folderX, folderY, pathX)
+        flag = dataLoaderPool.apply_async(save_each_nifty, args=oz)
         print("==>Current PID: ", flag.get(), "finished. ")
 
 dataLoaderPool.close()
