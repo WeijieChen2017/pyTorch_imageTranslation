3f19d918da3131251ec66e2ac6714fdf05276f2a
WeijieChen2017 <weijiechen1997@gmail.com>
2021-09-28 12:36:13 -0500

    Good morning!

--- eval.py
+++ eval.py
@@ -14,6 +14,7 @@ from data import DatasetFromFolder
 
 parser = argparse.ArgumentParser(description='Use 3d Unet to translate NAC PET to CT')
 parser.add_argument('--batch_size', type=int, default=1, help='training batch size')
+parser.add_argument('--loss_batch_cnt', type=int, default=100, help='eval loss batch size')
 parser.add_argument('--data_worker', type=int, default=4, help='number of threads for data loader to use')
 parser.add_argument('--seed', type=int, default=813, help='random seed to use.')
 parser.add_argument('--cuda', action='store_true', help='use cuda?')
@@ -55,20 +56,26 @@ model = torch.load(opt.model_save_path).to(device)
 model.eval()
 print("===> The model {} are loaded.".format(opt.model_save_path))
 
-
-epoch_loss = 0
-epoch_loss_list = []
-for iteration, batch in enumerate(dataloader_test, 1):
+epoch_loss = np.zeros((len(dataloader_test)))
+loss_batch = np.zeros((opt.loss_batch_cnt))
+n_samples = len(dataloader_test)
+for iteration, batch in enumerate(dataloader_test, 0):
     batch_x, batch_y, filename = batch[0].to(device), batch[1].to(device), batch[2]
     sample_name = os.path.basename(filename[0][0])
     sample_path = os.path.join(testSaveFolder, sample_name.replace("X", "pred"))
     pred = model(batch_x)
     np.save(sample_path, np.squeeze(pred.detach().numpy()))
-    loss = criterion(pred, batch_y)
-    epoch_loss += loss.item()
-    print("===> ({}/{}): Loss: {:.6f}".format(iteration, len(dataloader_test), loss.item()))
-    epoch_loss_list.append(loss.item())
+    loss = criterion(pred, batch_y).item() / opt.block_size ** 3
+    
+    loss_batch[iteration % loss_batch_cnt] = loss
+    epoch_loss[iteration] = loss
 
-print("The loss is ", epoch_loss / len(dataloader_test))
-np.save("val_loss.npy", np.asarray(epoch_loss_list))
+    if iteration % loss_batch_cnt == loss_batch_cnt - 1:
+        loss_mean = np.mean(loss_batch)
+        loss_std = np.std(loss_batch)
+        print("===> Eval({}/{}): ".format(iteration + 1, n_samples), end='')
+        print("Loss mean: {:.6f} Loss std: {:.6f}".format(loss_mean, loss_std))
+
+print("The eval loss mean: ", np.mean(epoch_loss), " std: ", np.std(epoch_loss))
+np.save("eval_loss.npy", epoch_loss)
 
