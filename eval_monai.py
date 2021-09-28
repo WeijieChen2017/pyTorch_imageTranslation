66f37f43d735408ae05422b5c5db5e1766954635
WeijieChen2017 <weijiechen1997@gmail.com>
2021-09-27 22:04:12 -0500

    Good morning!

--- eval_monai.py
+++ eval_monai.py
@@ -11,10 +11,28 @@ from torch.utils.data import DataLoader
 from model import Net
 from data import DatasetFromFolder
 
+def normX(data):
+    data[data<0] = 0
+    data[data>6000] = 6000
+    data = data / 6000
+    return data
+
+def normY(data):
+    data[data<-1000] = -1000
+    data[data>3000] = 3000
+    data = (data + 1000) / 4000
+    return data
+
+def denormX(data):
+    return data * 6000
+
+def denormY(data):
+    return data * 4000 - 1000
+
 # training setting
 
 parser = argparse.ArgumentParser(description='Use 3d Unet to translate NAC PET to CT')
-parser.add_argument('--batch_size', type=int, default=1, help='training batch size')
+parser.add_argument('--batch_size', type=int, default=10, help='training batch size')
 parser.add_argument('--data_worker', type=int, default=4, help='number of threads for data loader to use')
 parser.add_argument('--seed', type=int, default=813, help='random seed to use.')
 parser.add_argument('--cuda', action='store_true', help='use cuda?')
@@ -29,13 +47,17 @@ torch.manual_seed(opt.seed)
 device = torch.device("cuda" if opt.cuda else "cpu")
 
 # set the dataset
+filenameX = "011"
 testX = "./data_train/RSPET/RS_011.nii.gz"
 testY = "./data_train/RSCT/RS_011.nii.gz"
-testSave = "./data_pred/"
+dirSave = "./data_pred/"
 
 fileX = nib.load(testX)
 fileY = nib.load(testY)
-print("Shape X: ", fileX.shape, " Shape Y: ", fileY)
+dataX = fileX.get_fdata()
+dataY = fileY.get_fdata()
+inputX = normX(np.expand_dims(dataX, axis=(0, 1)))
+print("Shape X: ", dataX.shape, " Shape Y: ", dataY.shape, " Input X: ", inputX.shape)
 
 print("===> Datasets and Dataloders are set")
 
@@ -45,8 +67,36 @@ model = torch.load(opt.model_save_path).to(device)
 model.eval()
 print("===> The model {} are loaded.".format(opt.model_save_path))
 
-# monai.inferers.sliding_window_inference(inputs, roi_size, sw_batch_size, predictor, overlap=0.25, mode=BlendMode.CONSTANT, sigma_scale=0.125, padding_mode=PytorchPadMode.CONSTANT, cval=0.0, sw_device=None, device=None, *args, **kwargs)[source]¶
+pred = monai.inferers.sliding_window_inference(
+        inputs=inputX.to(device), #NCHW[D]
+        roi_size=opt.block_size, 
+        sw_batch_size=opt.batch_size, 
+        predictor=model, 
+        overlap=0.24, 
+        mode=BlendMode.CONSTANT, 
+        sigma_scale=0.125, # not valid if constant 
+        padding_mode=PytorchPadMode.CONSTANT, 
+        cval=0.0, 
+        sw_device=None, 
+        device=None)
 
+print("The loss is ", criterion(pred, normY(dataY)).item())
+
+data_pred = denormY(pred)
+data_diff = dataY - data_pred
+pred_file = nib.Nifti1Image(data_pred, fileX.affine, fileX.header)
+diff_file = nib.Nifti1Image(data_diff, fileX.affine, fileX.header)
+pred_name = dirSave+"pred_"+filenameX+".nii.gz"
+diff_name = dirSave+"diff_"+filenameX+".nii.gz"
+nib.save(pred_file, pred_name)
+nib.save(diff_file, diff_name)
+
+cmd0 = "3dresample -dxyz 1.367 1.367 3.27 -prefix DLCT_011_.nii.gz -input pred_011.nii.gz"
+cmd1 = "3dresample -dxyz 1.367 1.367 3.27 -prefix DMAP_011_.nii.gz -input diff_011.nii.gz"
+
+for cmd in [cmd0, cmd1]:
+    print(cmd0)
+    os.system(cmd0)
 
 
 # epoch_loss = 0
